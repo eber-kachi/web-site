51c7b64a5c58a3c0316dea75b87ac6a3de129c49
eber-kachi <eber.nina96@gmail.com>
2022-08-08 18:50:44 -0400

    taildwild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "accent-1": "#333",
+      },
     },
-    purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            colors: {
-                'accent-1': '#333',
-            },
-        },
-    },
-    variants: {},
-    plugins: [],
-}
+  },
+  variants: {},
+  plugins: [],
+};
