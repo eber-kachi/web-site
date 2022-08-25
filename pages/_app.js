f8d056acd93bf8a3ba1d1267732556499811f355
eber-kachi <eber.nina96@gmail.com>
2022-08-25 11:04:29 -0400

    configurando google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Script from "next/script";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Component {...pageProps} />
+            {/*se ejecuta una ves se carga la pagina */}
+            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-673TEBJSGT"/>
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-673TEBJSGT');
+                `}
+            </Script>
+        </>
+    )
 }
 
 export default MyApp
