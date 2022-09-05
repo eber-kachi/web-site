5540d582b77165838e207678c5baf3df64a3f2f5
eber-kachi <eber.nina96@gmail.com>
2022-09-05 12:44:37 -0400

    cabiando server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const next = require("next");
 
+const { join } = require("path");
+const { parse } = require("url");
+
 const port = parseInt(process.env.PORT, 10) || 80;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -9,11 +12,20 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
+  // redirects.forEach(({ from, to, type = 301, method = "get" }) => {
+  //   server[method](from, (req, res) => {
+  //     res.redirect(type, to);
+  //   });
+  // });
 
-  server.listen(port, () => {
+  server.get("/static", (req, res) =>
+    ssrCache({ req, res, pagePath: "/static" })
+  );
+
+  server.get("*", (req, res) => handle(req, res));
+
+  server.listen(port, (err) => {
+    if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
