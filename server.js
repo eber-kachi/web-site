5d9bdbe9f5d01325f138b8db0591cd57127f26c1
eber-kachi <eber.nina96@gmail.com>
2022-09-05 16:51:34 -0400

    probando 3

--- server.js
+++ server.js
@@ -1,51 +1,19 @@
-const express = require("express");
-const next = require("next");
-
-const { join } = require("path");
-const { parse } = require("url");
-
-const port = parseInt(process.env.PORT, 10) || 80;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  const server = express();
-
-  // redirects.forEach(({ from, to, type = 301, method = "get" }) => {
-  //   server[method](from, (req, res) => {
-  //     res.redirect(type, to);
-  //   });
-  // });
-
-  server.get("/static", (req, res) =>
-    ssrCache({ req, res, pagePath: "/static" })
-  );
-
-  server.get("*", (req, res) => handle(req, res));
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
-
-const { createServer } = require('http')
-const { parse } = require('url')
+const {createServer} = require('http')
+const {parse} = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = process.env.PORT || 3000
+const hostname = 'inittsoft.com'
+const port = process.env.PORT || 3000;
 
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+const app = next({dev:false, hostname, port });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     createServer(async (req, res) => {
         try {
             const parsedUrl = parse(req.url, true)
-            const { pathname, query } = parsedUrl
+            const {pathname, query} = parsedUrl
 
             if (pathname === '/a') {
                 await app.render(req, res, '/a', query)
@@ -63,4 +31,4 @@ app.prepare().then(() => {
         if (err) throw err
         console.log(`> Ready on http://${hostname}:${port}`)
     })
-})
+});
