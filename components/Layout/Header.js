39b5b1730be6dd870d1a5a3482621ec3f36c6c0c
Ronald <eronzarate@gmail.com>
2023-03-18 10:02:37 -0400

    Correccion ruta img

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
             <Image
-              src="/public/assets/Logo.png"
+              src="/assets/Logo.png"
               height={35}
               width={35}
               alt="Logo Initt"
