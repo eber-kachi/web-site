b8ec915054f5c98d4cdb0dd5d1c7d17a3e35e769
Ronald <eronzarate@gmail.com>
2023-03-18 09:48:20 -0400

    Img se añadio

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import LogoVPN from "../../public/assets/Logo.svg";
 import Facebook from "../../public/assets/Icon/facebook.svg";
 import Tiktok from "../../public/assets/Icon/tiktok-icon.svg";
@@ -7,7 +8,14 @@ const Footer = () => {
     <div className="bg-white-300 pt-44 pb-24">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-          {/* <LogoVPN className="h-8 w-auto mb-6" /> */}
+          <div>
+            <Image
+              src="/assets/Logo.png"
+              height={40}
+              width={40}
+              alt="Icon People"
+            />
+          </div>
           <p className="mb-4">
             <strong className="font-medium">InittSoft</strong> Es una empresa de
             tecnología enfocada en el uso de herramientas modernas integrando
